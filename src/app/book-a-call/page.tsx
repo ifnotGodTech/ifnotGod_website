026e54b20bc108edb5ac0452b6ef760317c7f077
FAITH <faithokereke26@gmail.com>
2025-08-25 15:11:12 +0100

    change cal to light mode

--- src/app/book-a-call/page.tsx
+++ src/app/book-a-call/page.tsx
@@ -40,8 +40,8 @@ export default function BookingPage() {
     <Cal
       namespace="secret"
       calLink="ifnotgodtech/30min"
-      style={{ width: "100%", height: "100%", overflow: "scroll" }}
-      config={{ layout: "month_view" }}
+      style={{ width: "100%", height: "100%", overflow: "scroll"  }}
+      config={{ layout: "month_view", theme: "light" }}
     />
   );
 }
