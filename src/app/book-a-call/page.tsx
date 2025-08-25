d1a1f063235377ba68bfcb73e491e5c1e693c873
FAITH <faithokereke26@gmail.com>
2025-08-25 14:15:59 +0100

    added cal

--- src/app/book-a-call/page.tsx
+++ src/app/book-a-call/page.tsx
@@ -1,28 +1,47 @@
 "use client";
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const BookingPage = () => {
+// const BookingPage = () => {
+//   useEffect(() => {
+//     if (!document.querySelector('script[data-tidycal="embed"]')) {
+//       const script = document.createElement("script");
+//       script.src = "https://asset-tidycal.b-cdn.net/js/embed.js";
+//       script.async = true;
+//       script.setAttribute("data-tidycal", "embed");
+//       document.body.appendChild(script);
+//     }
+//   }, []);
+
+//   return (
+//     <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+//       <iframe
+//         src="https://tidycal.com/faithi/15-minute-meeting"
+//         width="100%"
+//         height="100%"
+//         frameBorder="0"
+//         style={{ border: "none" }}
+//       ></iframe>
+//     </div>
+//   );
+// };
+
+// export default BookingPage;
+
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function BookingPage() {
   useEffect(() => {
-    if (!document.querySelector('script[data-tidycal="embed"]')) {
-      const script = document.createElement("script");
-      script.src = "https://asset-tidycal.b-cdn.net/js/embed.js";
-      script.async = true;
-      script.setAttribute("data-tidycal", "embed");
-      document.body.appendChild(script);
-    }
+    (async function () {
+      const cal = await getCalApi({ namespace: "secret" });
+      cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+    })();
   }, []);
-
   return (
-    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
-      <iframe
-        src="https://tidycal.com/faithi/15-minute-meeting"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        style={{ border: "none" }}
-      ></iframe>
-    </div>
+    <Cal
+      namespace="secret"
+      calLink="ifnotgodtech/30min"
+      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+      config={{ layout: "month_view" }}
+    />
   );
-};
-
-export default BookingPage;
+}
